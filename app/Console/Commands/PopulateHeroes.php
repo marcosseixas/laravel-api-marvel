970be688cb6f2cefae33737d3dd8a73d907e680c
Marcos Seixas <seixas.secret@gmail.com>
2022-10-28 19:08:18 -0300

    modificações api marvel

--- app/Console/Commands/PopulateHeroes.php
+++ app/Console/Commands/PopulateHeroes.php
@@ -3,8 +3,6 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
-use Illuminate\Support\Facades\Http;
-use App\Services\GetTokenService;
 
 class PopulateHeroes extends Command
 {
@@ -22,19 +20,44 @@ class PopulateHeroes extends Command
      */
     protected $description = 'Chama a api da marvel e popula o banco de dados com os herois';
 
+    protected $link = 'https://gateway.marvel.com:443';
+
     /**
      * Execute the console command.
      *
      * @return int
      */
-    public function handle()
+    public function handle($infos = [])
     {
-        $token = new GetTokenService();
-        $token = $token->getToken();
-        dd($token);
-        $response = Http::get('http://example.com');
+        $ts = time();
+        $privateKey = env('PRIVATE_KEY');
+        $apikey = env('PUBLIC_KEY');
+        $hash = md5($ts . $privateKey . $apikey);
 
-        dd($response);
+        $client = new \GuzzleHttp\Client();
+        $response = $client->request('GET', $this->link . '/v1/public/characters?ts=' . $ts . '&apikey=' . $apikey . '&hash=' . $hash . '&limit=2', []);
+
+        $status = $response->getStatusCode();
+        $results = json_decode($response->getBody());
+        if($status == 200)
+        {
+            foreach($results->data->results as $hero) {
+
+                $img = $hero->thumbnail->path . '.' . $hero->thumbnail->extension;
+                $infos[] = [
+                    'id' => $hero->id,
+                    'name' => $hero->name,
+                    'img' => $img
+                ];
+        
+            }
+           
+            dd($infos);
+        }
+
+        dd('end');
+
+
         return Command::SUCCESS;
     }
 }
